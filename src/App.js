a6af03d44a4db71f0f46e5f6d3ad124fc1753bfd
Nina Loeterman <nina.loeterman@gmail.com>
2021-04-24 10:51:01 +0300

    changed default export

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import './App.css';
-import Button from './components/Button/Button';
+// import './App.css';
+// import Button from './components/Button/Button';
 
-function App() {
+// function App() {
 
-  const doSomething = () => {
-    console.log('im doing a lot of things');
-  }
+//   const doSomething = () => {
+//     console.log('im doing a lot of things');
+//   }
 
-  return (
-    <div className="App">
-      <Button type="bookmark" onClick={doSomething} theme="accent" className="new_style" />
-      <Button title="click me" theme="light" outlined />
-      <Button type="love" theme="dark" />
-      <Button title="send" type="send" theme="accent" />
-      <Button title="lets chat" type="chat" theme="light" />
-    </div>
-  );
-}
+//   return (
+//     <div className="App">
+//       <Button type="bookmark" onClick={doSomething} theme="accent" className="new_style" />
+//       <Button title="click me" theme="light" outlined />
+//       <Button type="love" theme="dark" />
+//       <Button title="send" type="send" theme="accent" />
+//       <Button title="lets chat" type="chat" theme="light" />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
